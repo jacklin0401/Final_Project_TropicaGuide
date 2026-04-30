5dc673ac87f52aac7bb012090dc2eb569faffeb4
Caira Major <cairamajor@gmail.com>
2026-04-30 13:45:46 -0400

    update auth service to create Firestore user profile on signup

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,16 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   Stream<User?> get authStateChanges => _auth.authStateChanges();
 
-  Future<User?> signUp(String email, String password) async {
+  User? get currentUser => _auth.currentUser;
+
+  Future<User?> signUp(
+    String email,
+    String password,
+    String fullName,
+  ) async {
     final result = await _auth.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
-    return result.user;
+
+    final user = result.user;
+
+    if (user != null) {
+      await user.updateDisplayName(fullName);
+
+      
+      await _db.collection('users').doc(user.uid).set({
+        'uid': user.uid,
+        'fullName': fullName,
+        'email': email,
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+    }
+
+    return user;
   }
 
   Future<User?> signIn(String email, String password) async {
@@ -24,6 +47,4 @@ class AuthService {
   Future<void> signOut() async {
     await _auth.signOut();
   }
-
-  User? get currentUser => _auth.currentUser;
 }
